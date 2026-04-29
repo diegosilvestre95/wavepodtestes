3f88d010d24e800d00ced47f3fd9ca2e4d07139b
diegosilvestre95 <diego.silvestre@vettait.com.br>
2026-04-29 11:49:55 -0300

    fix: import Link in Header.jsx to resolve ReferenceError

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useApp } from '../context/AppContext'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
 export default function Header({ showAdminBtn = false, showLogout = false }) {
   const { currentUser, logout } = useApp()
